819f6ba9c444f5c8daa6157ab57a9d00e707a66a
ImMahanBro <149080672+MahanMalekjary@users.noreply.github.com>
2026-06-18 14:39:55 +0330

    Username,MSGDELETE,Edit,Reply

--- app.js
+++ app.js
@@ -5,28 +5,126 @@ const supabaseKey = "sb_publishable_iDTlvh6uyYGmxV-2ubBw7g_mu1QBKTb";
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 let username = "";
+let ownerId = "";
+let replyToId = null;
+let replyToText = null;
+let replyToName = null;
+let unreadMentions = [];
+let editingMessageId = null;
+
+
+
+
+function generateUsername() {
+
+  const number =
+    Math.floor(
+      1000 + Math.random() * 9000
+    );
+
+  const letter =
+    String.fromCharCode(
+      65 +
+      Math.floor(
+        Math.random() * 26
+      )
+    );
+
+  return `LM${number}${letter}`;
+}
+
+
+
+
+
+
+async function generateUniqueUsername() {
+
+  while (true) {
+
+    const candidate =
+      "LM" +
+      Math.floor(
+        1000 +
+        Math.random() * 9000
+      );
+
+    const { data } =
+      await supabaseClient
+        .from("active_users")
+        .select("name")
+        .eq("name", candidate)
+        .maybeSingle();
+
+    if (!data) {
+      return candidate;
+    }
+  }
+}
+
+
+
+
+
+
 
 // ورود به چت
 async function enterChat() {
-  const input = document.getElementById("nameInput").value.trim();
-  if (!input) return alert("Enter name!");
 
-  // check user
-  const { data, error } = await supabaseClient
-    .from("active_users")
-    .select("*")
-    .eq("name", input)
-    .maybeSingle();
+  let savedName =
+    localStorage.getItem(
+      "copyright_username"
+    );
 
-  if (error) {
-    alert("Error!");
-    return;
+  if (!savedName) {
+
+    savedName =
+      generateUsername();
+
+    localStorage.setItem(
+      "copyright_username",
+      savedName
+    );
   }
 
-  username = input;
+  username = savedName;
 
-  document.getElementById("loginBox").classList.add("hidden");
-  document.getElementById("chatBox").classList.remove("hidden");
+
+  let savedOwnerId =
+    localStorage.getItem(
+      "copyright_owner_id"
+    );
+
+  if (!savedOwnerId) {
+
+    savedOwnerId =
+      crypto.randomUUID();
+
+    localStorage.setItem(
+      "copyright_owner_id",
+      savedOwnerId
+    );
+  }
+
+  ownerId = savedOwnerId;
+
+
+  document
+    .getElementById("loginBox")
+    .classList
+    .add("hidden");
+
+  document
+    .getElementById("chatBox")
+    .classList
+    .remove("hidden");
+
+
+
+  document
+    .getElementById("myUsername")
+    .textContent =
+    `You: ${username}`;
 
   loadMessages();
   subscribeMessages();
@@ -34,15 +132,18 @@ async function enterChat() {
 
 // ارسال پیام
 async function sendMessage() {
+
   const msgInput = document.getElementById("msgInput");
   const fileInput = document.getElementById("fileInput");
+
+  const message =
+    msgInput.value.trim();
 
   const sendBtn = document.getElementById("sendBtn");
 
   sendBtn.disabled = true;
   sendBtn.textContent = "Sending...";
 
-  const message = msgInput.value;
   let mediaUrl = null;
   let mediaType = null;
 
@@ -59,16 +160,61 @@ async function sendMessage() {
     }
   }
 
+  if (editingMessageId !== null) {
+
+
+    console.log(
+  "EDITING ID:",
+  editingMessageId
+);
+
+  const { data, error } =
+    await supabaseClient
+      .from("messages")
+      .update({
+        message: message,
+        edited: true
+      })
+      .eq("id", editingMessageId)
+      .select();
+
+  console.log("EDIT RESULT:", data);
+  console.log("EDIT ERROR:", error);
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  editingMessageId = null;
+
+  msgInput.value = "";
+
+  sendBtn.disabled = false;
+  sendBtn.textContent = "Send";
+
+  loadMessages();
+
+  return;
+}
+
+
   await supabaseClient
     .from("messages")
     .insert([
       {
         name: username,
+        owner_id: ownerId,
         message: message,
+        reply_to_id: replyToId,
+        reply_to_text: replyToText,
+        reply_to_name: replyToName,
         media_url: mediaUrl,
         media_type: mediaType
       }
     ]);
+
+  cancelReply();
 
   msgInput.value = "";
   fileInput.value = "";
@@ -89,35 +235,187 @@ async function loadMessages() {
 
 // نمایش پیام‌ها
 function renderMessages(messages) {
-  const box = document.getElementById("messages");
+
+  const box =
+    document.getElementById("messages");
+
   box.innerHTML = "";
 
   messages.forEach(m => {
-    const div = document.createElement("div");
+
+    const div =
+      document.createElement("div");
+
+    div.id = `msg-${m.id}`;
+
     div.className = "msg";
+
+    const isMine =
+      m.owner_id === ownerId;
+
+
+
+    if (isMine) {
+      div.classList.add("self");
+    }
 
     let mediaHTML = "";
 
     if (m.media_type === "image") {
-      mediaHTML = `<br><img src="${m.media_url}" style="max-width:200px;border-radius:10px;">`;
+      mediaHTML =
+        `<br><img src="${m.media_url}" style="max-width:200px;border-radius:10px;">`;
     }
 
     if (m.media_type === "video") {
-      mediaHTML = `<br><video src="${m.media_url}" controls style="max-width:200px;border-radius:10px;"></video>`;
+      mediaHTML =
+        `<br><video src="${m.media_url}" controls style="max-width:200px;border-radius:10px;"></video>`;
     }
 
+    // اول ساخته میشه
+
+
+    let editBtn = "";
+
+    if (isMine) {
+
+      editBtn = `
+  <button
+    class="editBtn"
+    onclick="startEdit(
+      ${m.id},
+      \`${(m.message || "").replace(/`/g, "\\`")}\`
+    )"
+  >
+    ✏️
+  </button>
+  `;
+    }
+
+
+    let deleteBtn = "";
+
+    let replyBtn = `
+<button
+class="replyBtn"
+onclick="setReply(
+${m.id},
+'${(m.message || "").replace(/'/g, "\\'")}',
+'${m.name}'
+)">
+↩
+</button>
+`;
+
+    if (isMine) {
+
+      deleteBtn = `
+      <button
+        class="deleteBtn"
+        onclick="deleteMessage(${m.id})"
+      >
+        🗑
+      </button>
+      `;
+    }
+
+    let replyHTML = "";
+
+    if (m.reply_to_id) {
+
+      replyHTML = `
+<div
+  class="replyPreview clickableReply"
+  onclick="jumpToMessage(${m.reply_to_id})"
+>
+
+  <b>${m.reply_to_name}</b>
+
+  <br>
+
+  ${m.reply_to_text}
+
+</div>
+`;
+    }
+
+    let formattedMessage =
+      m.message || "";
+
+    formattedMessage =
+      formattedMessage.replace(
+        /@([a-zA-Z0-9_]+)/g,
+        '<span class="mention">@$1</span>'
+      );
+
+
+    // بعد استفاده میشه
+
+
+    let editedHTML = "";
+
+    if (m.edited) {
+
+      editedHTML =
+        `<div class="editedLabel">
+     edited
+   </div>`;
+    }
+
+
+
     div.innerHTML = `
-      <span class="name">${m.name}:</span> ${m.message || ""}
-      ${mediaHTML}
-    `;
+  <div class="msgHeader">
+
+    <span class="name">
+      ${m.name}
+    </span>
+
+    ${replyBtn}
+${editBtn}
+${deleteBtn}
+
+  </div>
+
+  ${replyHTML}
+
+  ${formattedMessage}
+
+${mediaHTML}
+
+${editedHTML}
+`;
+
+
+    const mentionTag = `@${username}`;
+
+    if (
+      m.message &&
+      m.message.includes(mentionTag)
+    ) {
+
+      div.classList.add("mentioned");
+
+      if (
+        m.owner_id !== ownerId &&
+        !unreadMentions.includes(m.id)
+      ) {
+
+        unreadMentions.push(m.id);
+
+        updateMentionBadge();
+      }
+    }
+
 
     box.appendChild(div);
+
   });
 
-  box.scrollTop = box.scrollHeight;
+
+
+  box.scrollTop =
+    box.scrollHeight;
 }
-
-
 
 
 async function uploadMedia(file) {
@@ -197,6 +495,180 @@ function fakeProgress() {
 
 
 
+async function deleteMessage(id) {
+
+  console.log("DELETE CLICKED:", id);
+
+  const ok = confirm("Delete this message?");
+
+  if (!ok) return;
+
+  const { data, error } =
+    await supabaseClient
+      .from("messages")
+      .delete()
+      .eq("id", id)
+      .select();
+
+  console.log("DELETE RESULT:", data);
+  console.log("DELETE ERROR:", error);
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  loadMessages();
+}
+
+
+
+function setReply(id, text, name) {
+
+  replyToId = id;
+
+  replyToText = text;
+
+  replyToName = name;
+
+  document
+    .getElementById("replyBox")
+    .classList
+    .remove("hidden");
+
+  document
+    .getElementById("replyInfo")
+    .innerHTML =
+    `<b>${name}</b>: ${text}`;
+}
+
+function cancelReply() {
+
+  replyToId = null;
+  replyToText = null;
+  replyToName = null;
+
+  document
+    .getElementById("replyBox")
+    .classList
+    .add("hidden");
+}
+
+
+function jumpToMessage(id) {
+
+  const target =
+    document.getElementById(`msg-${id}`);
+
+  if (!target) return;
+
+  target.scrollIntoView({
+    behavior: "smooth",
+    block: "center"
+  });
+
+  target.classList.add("highlighted");
+
+  setTimeout(() => {
+
+    target.classList.remove(
+      "highlighted"
+    );
+
+  }, 2000);
+
+}
+
+
+
+function updateMentionBadge() {
+
+  const badge =
+    document.getElementById(
+      "mentionBadge"
+    );
+
+  const count =
+    document.getElementById(
+      "mentionCount"
+    );
+
+  count.textContent =
+    unreadMentions.length;
+
+  if (unreadMentions.length > 0) {
+
+    badge.classList.remove(
+      "hidden"
+    );
+
+  } else {
+
+    badge.classList.add(
+      "hidden"
+    );
+  }
+}
+
+document
+  .getElementById("mentionBadge")
+  .addEventListener(
+    "click",
+    () => {
+
+      if (
+        unreadMentions.length === 0
+      ) return;
+
+      const lastMention =
+        unreadMentions[
+        unreadMentions.length - 1
+        ];
+
+      jumpToMessage(
+        lastMention
+      );
+
+      unreadMentions = [];
+
+      updateMentionBadge();
+
+    }
+  );
+
+
+
+
+
+
+
+function startEdit(id, text) {
+
+   console.log("START EDIT ID:", id);
+
+  editingMessageId = id;
+
+  editingMessageId = id;
+
+  document
+    .getElementById("msgInput")
+    .value = text;
+
+  const sendBtn =
+    document.getElementById("sendBtn");
+
+  sendBtn.textContent = "Edit";
+
+  document
+    .getElementById("msgInput")
+    .focus();
+}
+
+
+
+
+
+
 // realtime listener
 function subscribeMessages() {
   supabaseClient
@@ -204,11 +676,11 @@ function subscribeMessages() {
     .on(
       "postgres_changes",
       {
-        event: "INSERT",
+        event: "*",
         schema: "public",
         table: "messages"
       },
-      (payload) => {
+      () => {
         loadMessages();
       }
     )
